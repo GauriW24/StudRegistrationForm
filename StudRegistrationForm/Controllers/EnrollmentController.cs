82f0909c84bcf454d7266c4f067a85ede9f960db
Gauri Wadekar <gauriwadekar2417@gmail.com>
2024-06-02 16:49:29 +0530

    Add project files.

---
+++ StudRegistrationForm/Controllers/EnrollmentController.cs
@@ -0,0 +1,68 @@
+﻿using StudRegistrationForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+
+namespace StudRegistrationForm.Controllers
+{
+    public class EnrollmentController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(StudentReg s)
+        {
+            if (ModelState.IsValid)
+            {
+                //StudentReg sr = new StudentReg();
+                SqlConnection con;
+                try
+                {
+                    string connectionString = ConfigurationManager.ConnectionStrings["MyDatabase"].ConnectionString;
+
+                    using (con = new SqlConnection(connectionString))
+                    {
+                        using (SqlCommand cmd = new SqlCommand("studentInfo..SP_EnrollDetail", con))
+                        {
+                            cmd.Parameters.Clear();
+                            cmd.CommandType = CommandType.StoredProcedure;
+                            cmd.Parameters.AddWithValue("@name", s.name);
+                            cmd.Parameters.AddWithValue("@email", s.email);
+                            cmd.Parameters.AddWithValue("@MobileNo", s.mobno);
+                            cmd.Parameters.AddWithValue("@status", "Insert");
+
+                            con.Open();
+                            int result= cmd.ExecuteNonQuery();
+                            cmd.Dispose();
+                            con.Close();
+                            
+                            return RedirectToAction("Index"); // Redirect to the same action to clear the form
+
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ViewData["error"] = ex;
+                }
+
+                return View(s);
+
+            }
+            return View();
+        }
+        
+
+    }
+}
+
---
+++ StudRegistrationForm/Models/StudentReg.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudRegistrationForm.Models
+{
+    public class StudentReg
+    {
+        [Required(ErrorMessage ="Please enter name!")]
+        public string name { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[^\s@]+@[^\s@]+\.[^\s@]+$", ErrorMessage = "Invalid email!")]
+        public string email { get; set; }
+
+        [Required(ErrorMessage ="Please enter mobile number!")]
+        //[RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})", ErrorMessage = "Invalid mobile number!")]
+        public string mobno { get; set; }
+
+        //public List<StudentReg> studRegInfo { get; set; }
+
+    }
+}
